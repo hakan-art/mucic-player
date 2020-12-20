5d418de196a7b6369fefbc8bd46394b4536c3f93
hakan-art <hakan.gezer74@googlemail.com>
2020-12-20 10:35:26 +0100

    background body

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Library from "./components/Library";
 import "./styles/app.scss";
 // Import Util
 import data from "./util";
+import Nav from "./components/Nav";
 
 function App() {
   // Ref
@@ -19,6 +20,7 @@ function App() {
     currentTime: 0,
     duration: 0,
   });
+  const [libraryStatus, setLibraryStatus] = useState(false)
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
@@ -26,7 +28,8 @@ function App() {
     setSongInfo({ ...songInfo, currentTime: current, duration });
   };
   return (
-    <div className="App">
+    <div className={`App ${libraryStatus ? 'active-App' : ''}`} >
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong} />
       <Player
         audioRef={audioRef}
@@ -36,14 +39,14 @@ function App() {
         setSongInfo={setSongInfo}
         songInfo={songInfo}
       />
-      <Library songs={songs} setCurrentSong={setCurrentSong} audioRef={audioRef} isPlaying={isPlaying} setSongs={setSongs} />
+      <Library songs={songs} setCurrentSong={setCurrentSong} audioRef={audioRef} isPlaying={isPlaying} setSongs={setSongs} libraryStatus={libraryStatus} />
       <audio
         onTimeUpdate={timeUpdateHandler}
         onLoadedMetadata={timeUpdateHandler}
         ref={audioRef}
         src={currentSong.audio}
       ></audio>
-    </div>
+    </ div>
   );
 }
 
